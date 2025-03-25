08d8f89b9d6982ac2adf4c1e44f65be958a5dddd
Kgomotso Mbele <kgomotsombele@outlook.com>
2025-03-25 17:08:45 +0200

    Added food suggestions Added an error toast Added better GUI Made buttons operate Made User Input clear

--- app/src/main/java/com/example/st10467825_assignment1/MainActivity.kt
+++ app/src/main/java/com/example/st10467825_assignment1/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.st10467825_assignment1
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,41 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        //i opted to declare all my values in one spot for cleaner code
+        val editUserText = findViewById<EditText>(R.id.editUserText)
+        val buttonGenerate = findViewById<Button>(R.id.buttonGenerate)
+        val buttonClear = findViewById<Button>(R.id.buttonClear)
+        val textViewOutput = findViewById<TextView>(R.id.textViewOutput)
+
+        //this is where my button does something i think
+        buttonGenerate.setOnClickListener{
+            var input = editUserText.text.toString().trim().toLowerCase()
+
+            when{//all my possible accepted inputs are stacked together as well for cleaner code
+                input=="morning" -> textViewOutput.text = "Omelette or Oats with fruit"
+                input=="mid morning" -> textViewOutput.text = "Bagel or Muffins"
+                input=="mid-morning" -> textViewOutput.text = "Greek Salad with Chicken Strips"
+                input=="afternoon" -> textViewOutput.text = "Chicken Mayo Sandwich"
+                input=="mid afternoon" -> textViewOutput.text = "Steak and Kidney Pie"
+                input=="mid-afternoon" -> textViewOutput.text = "Milk Tart slice" //personal favourite
+                input=="evening" -> textViewOutput.text = "Sirloin Steak with Mashed Potatoes"
+                input=="midnight snack" -> textViewOutput.text = "Apple or Banana"
+                input=="snack" -> textViewOutput.text = "Kale chips"
+                else -> Toast.makeText(this, "Nice try dude, put something else in", Toast.LENGTH_LONG).show()
+            } //don't question the food choice, i just went off the top of my head
+            //from here, my 'reset suggestions' button clears the user input and suggestions box. figuring this out was difficult
+            buttonClear.setOnClickListener{
+                editUserText.text.clear()
+                textViewOutput.text = ""
+
+
+            }
+
         }
+
+
+
+
+
     }
 }
